c6fcdb6e925b30e89981248dbabf3fb8a1054600
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-02-22 16:25:49 -0700

    tried to follow along

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', handleSubmit)
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e.target['new-task-description'].value)
+    const li = document.createElement('li')
+    li.textContent = e.target['new-task-description'].value
+    const task = document.querySelector("#tasks")
+    task.append(li)
+    // li.className = 
+    const button = document.createElement("button")
+    button.textContent = 'x'
+    li.append(button)
+    button.addEventListener('click', () => {
+      li.remove()
+    })
+  }
 });
